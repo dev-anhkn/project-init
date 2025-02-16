42ed6c0ef174857b76929f23218451969be0fe2f
Kim Ngọc Anh <>
2025-02-16 16:28:20 +0700

    conflic

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # project-init
+<<<<<<< HEAD
 init-project
+=======
+- springframework :3.4.1
+- gradle: 8.4
+- JavaVersion: VERSION_17
+- springdoc-openapi
+- security
+>>>>>>> 8f04f3c (init)
---
+++ demo-service/src/main/java/project/init/account/DemoServiceApplication.java
@@ -0,0 +1,13 @@
+package project.init.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"project.init.account", "project.init.framework"})
+public class DemoServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoServiceApplication.class);
+    }
+}
